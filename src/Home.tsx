0f6796ed3f68d60ad98691fad6ae8a0b6cf5365c
RichardTMiles <9538357+RichardTMiles@users.noreply.github.com>
2024-04-17 07:08:27 +0000

    Deploying to gh-pages from @ MilesSystems/professionmate.com@bc3716048be683dd504ab5dd96467673aca94c7a 🚀

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,14 +1,16 @@
 import {IconProp} from "@fortawesome/fontawesome-svg-core";
+import {faAddressBook, faChess, faPalette, faPhone} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faPalette, faAddressBook, faChess, faPhone} from "@fortawesome/free-solid-svg-icons";
+import dalle10 from 'assets/dalle10.webp';
+import dalle11 from 'assets/dalle11.webp';
+import dalle12 from 'assets/dalle12.webp';
+import dalle15 from 'assets/dalle15.webp';
+import dalle17 from 'assets/dalle17.webp';
 import dalle5 from 'assets/dalle5.webp';
 import dalle6 from 'assets/dalle6.webp';
 import dalle8 from 'assets/dalle8.webp';
 import dalle9 from 'assets/dalle9.webp';
-import dalle10 from 'assets/dalle10.webp';
-import dalle11 from 'assets/dalle11.webp';
-import dalle12 from 'assets/dalle12.webp';
-import Parallax from "src/components/Parallax";
+import Parallax, {eParallaxStyle} from "src/components/Parallax";
 
 
 interface iHomeCardBody {
@@ -29,7 +31,7 @@ const HomeCards: iHomeCardBody[] = [
     },
     {
         title: 'Mate',
-        icon:faAddressBook,
+        icon: faAddressBook,
         description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
         link: '#',
         image: dalle6
@@ -53,13 +55,13 @@ const HomeCards: iHomeCardBody[] = [
 export default function Home() {
 
     const allHomeCards = <div className={'row'} style={{
-        margin: '10px'
+        margin: '40px'
     }}>
-        {HomeCards.map(info => <div className={'col-sm-6 col-md-3'}>
+        {HomeCards.map(info => <div className={'col-xs-12 col-sm-6 col-md-3'}>
             <div className="card">
                 <img src={info.image} className="card-img-top" alt={info.title + ' image'}/>
                 <div className="card-body text-center" style={{
-                    backgroundColor: 'rgba(0,0,0,.2)',
+                    backgroundColor: 'rgba(28,116,140,0.27)',
                     color: 'black'
                 }}>
                     <div className={'row d-flex justify-content-center'}>
@@ -89,12 +91,12 @@ export default function Home() {
     </div>
 
     return <>
-        <Parallax backgroundImage={dalle10}>
+        <Parallax height={'75vh'} backgroundImage={dalle10}>
             <div className="container">
                 <div className={'row'}>
                     <div className={'col-md-6'}>
                         <div className={'card'} style={{
-                            backgroundColor: 'rgba(0,0,0,.2)',
+                            backgroundColor: 'rgba(51,169,204,0.27)',
                             color: 'whitesmoke'
                         }}>
                             <div className="card-body">
@@ -110,7 +112,9 @@ export default function Home() {
                 </div>
             </div>
         </Parallax>
-        {allHomeCards}
+        <Parallax height={'75vh'} backgroundImage={dalle17} parallaxStyle={eParallaxStyle.SCROLL}>
+            {allHomeCards}
+        </Parallax>
         <Parallax backgroundImage={dalle11}>
             <div className="container">
                 <div className={'row'}>
@@ -132,7 +136,28 @@ export default function Home() {
                 </div>
             </div>
         </Parallax>
-        {allHomeCards}
+        <Parallax backgroundImage={dalle15} parallaxStyle={eParallaxStyle.SCROLL}>
+            <div className="container">
+                <div className={'row'}>
+                    <div className={'col-md-6'}>
+                        <div className={'card'} style={{
+                            backgroundColor: 'rgba(51,169,204,0.27)',
+                            color: 'whitesmoke'
+                        }}>
+                            <div className="card-body">
+                                <h1>
+                                    Welcome to Profession Mate
+                                </h1>
+                                <p>
+                                    We Handle The Entire Job Searching and Applying Process For You!
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Parallax>
+
         <Parallax backgroundImage={dalle12}>
             <div className="container">
                 <div className={'row'}>
